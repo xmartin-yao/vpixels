d5398b0badfb774eb72323b76e5d605324e53914
xmartin <xmartinyao@gmail.com>
2021-01-03 16:44:37 -0500

    adopt newly added features into example/py/hourglass.py

--- example/py/hourglass.py
+++ example/py/hourglass.py
@@ -1,5 +1,5 @@
 #######################################################################
-# Copyright (C) 2019 Xueyi Yao
+# Copyright (C) 2019-2021 Xueyi Yao
 #
 # This file is part of VPixels.
 #
@@ -14,13 +14,17 @@
 # GNU General Public License for more details.
 # 
 # You should have received a copy of the GNU General Public License
-# along with VPixels.  If not, see <https://www.gnu.org/licenses/>.
+# along with VPixels. If not, see <https://www.gnu.org/licenses/>.
 #######################################################################
-import vpixels
+import vpixels as vp
 import time
 
+# check version of the module
+assert vp.version( '0.7.2' ), "require version 0.7.2 or later"
+
+
 # Generate a GIF file of animated hourglass
-class hourglass( vpixels.gif ):
+class hourglass( vp.gif ):
   def __init__( self ):
     # 2 bits/pixel, 99x99, 93 images
     super( hourglass, self ).__init__( 2, 99, 99, 93 )
@@ -43,10 +47,17 @@ class hourglass( vpixels.gif ):
 
   # set color table entries
   def __setcolor( self ):
+    # set color table entries
     self.setcolor( 0, 255, 255, 255 )  # white
     self.setcolor( 1, 255, 0, 0 )      # red
     self.setcolor( 2, 0, 255, 0 )      # green
     self.setcolor( 3, 0, 0, 255 )      # blue
+
+    # alias for each entry
+    self.white = 0
+    self.red   = 1
+    self.green = 2
+    self.blue  = 3
 
 
   # draw
@@ -55,65 +66,119 @@ class hourglass( vpixels.gif ):
     self.__layout()
 
     # image #0 ~ #46
-    self.__paint0_46()
+    self.__paint0to46()
 
     # image #47 ~ #49
     self.__reflect()
 
-    # set delay time of image #46 and #92
-    self[46].delay( 200 )
-    self[92].delay( 200 )
 
-
-  # draw two triangles for each image
+  # draw two blue triangles on each image
   def __layout( self ):
-    # image #0
-    img0 = self.getimage( 0 )
+    # draw image #0
+    img0 = self.__layoutimage0()
 
-    # draw image #0: two horizontal lines
-    for x in range( 2, 97 ):
-      img0.setpixel( x, 2, 3 )  # entry 3 is blue color
-      img0.setpixel( x, 96, 3 )
-
-    # draw image #0: two diagonal lines
-    for x in range( 3, 96 ):
-      y = x
-      img0.setpixel( x, y, 3 )
-      img0.setpixel( 98 - x, y, 3 )
-
-    # clone image #0
+    # copy image #0
     for i in range( 1, 93 ):
-      self[i].clone( img0 )
+      self[i] = img0
 
 
-  # image #0 ~ #46
-  def __paint0_46( self ):
-    # top triangle
-    offset = 3
-    for i in range( 0, 47 ):
-      img = self[i]
-      for y in range( offset, 49 ):
-        for x in range( y + 1, 98 - y ):
-          img.setpixel( x, y, 2 )
-      offset += 1
-
-    # bottom triangle
-    offset = 95
-    for i in range( 1, 47 ):
-      img = self[i]
-      for y in range( offset, 96 ):
-        for x in range( 98 - y + 1, y):
-          img.setpixel( x, y, 2 )
-      offset -= 1
+  # paint image #0 ~ #46
+  def __paint0to46( self ):
+    self.__painttoptriangle()
+    self.__paintbottomtriangle()
 
 
-  # image #47 ~ #49
-  # clone image #45 ~ image #0, symmetric about image #46
+  # copy image #45 ~ #0 to image #47 ~ #92
+  # symmetric about image #46
   def __reflect( self ):
     j = 45
     for i in range( 47, 93 ):
-      self[i].clone( self[j] )
+      self[i] = self[j]
       j -= 1
+
+
+  # draw two blue triangles on image #0
+  def __layoutimage0( self ):
+    # image #0
+    img0 = self[0]
+
+    # draw two blue horizontal lines
+    # (2,2)->(96,2) and (2,96)->(96,96)
+    for x in range( 2, 97 ):
+      img0.setpixel( x, 2,  self.blue )
+      img0.setpixel( x, 96, self.blue )
+
+    # draw two blue diagonal lines
+    # (3,3)->(95,95) and (95,3)->(3,95)
+    # intersect at (49,49), the very center of the image
+    for x in range( 3, 96 ):
+      y = x
+      img0.setpixel( x, y, self.blue )
+      img0.setpixel( 98 - x, y, self.blue )
+
+    return img0
+
+
+  # fill top triangle using color green
+  #   image #0: (4,3)->(94,3), ... (49,48), 46 lines, triangle fully filled
+  #   image #1: (5,4)->(93,4), ... (49,48), 45 lines
+  #   ...
+  #   image #44: (48,47)->(50,47), ... (49,48), 2 lines
+  #   image #45: (49,48), 1 line (just a pixel)
+  #   image #46: leave triangle empty
+  def __painttoptriangle( self ):
+    nlines = 46  # number of lines to draw
+
+    # paint image #0 ~ #45
+    for i in range( 46 ):
+      img = self[i]  # i-th iamge
+
+      # 1st line is just a pixel at (49,48)
+      # one line up the center of the image (49,49)
+      xstart, xend, y = 49, 49, 48
+
+      for line in range( nlines ):  # draw lines
+        for x in range( xstart, xend + 1):  # draw a line
+          img.setpixel( x, y, self.green )  # draw a pixel
+
+        # next line is two pixels wider and located one line up
+        xstart -= 1
+        xend += 1
+        y -= 1
+
+      # next image is one line less than current one
+      nlines -= 1
+
+
+  # fill bottom triangle using color green
+  #   image #0: leave triangle empty
+  #   image #1: (4,95)->(94,95), 1 line
+  #   image #2: (4,95)->(94,95), (5,94)->(93, 94), 2 lines
+  #   ...
+  #   image #45: (4,95)->(94,95), ... (48,51)->(50,51), 45 lines
+  #   image #46: (4,95)->(94,95), ... (49,50), 46 lines, triangle fully filled
+  def __paintbottomtriangle( self ):
+    nlines = 1  # number of lines to draw
+
+    # paint image #1 ~ #46
+    for i in range( 1, 47 ):
+      img = self[i]  # i-th iamge
+
+      # 1st horizontal line (4,95)->(94,95)
+      # one line up the bottom blue line
+      xstart, xend, y = 4, 94, 95
+
+      for line in range( nlines ):  # draw lines
+        for x in range( xstart, xend + 1 ):  # draw a line
+          img.setpixel( x, y, self.green )   # draw a pixel
+
+        # next line is two pixels shorter and located one line up
+        xstart += 1
+        xend -= 1
+        y -= 1
+
+      # next image has one more line than current one
+      nlines += 1
 
 
   # set delay time of images
